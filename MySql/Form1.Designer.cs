6f3508e81b7348322df4378a57a179f96c7161dd
Apollo12345678 <weiszapollo@gmail.com>
2018-12-03 15:01:52 +0100

    kész

---
+++ MySql/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace MySql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.NevTextBox = new System.Windows.Forms.TextBox();
+            this.FajtaTextBox = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(12, 39);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Hozzáadd";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // NevTextBox
+            // 
+            this.NevTextBox.Location = new System.Drawing.Point(13, 13);
+            this.NevTextBox.Name = "NevTextBox";
+            this.NevTextBox.Size = new System.Drawing.Size(100, 20);
+            this.NevTextBox.TabIndex = 1;
+            // 
+            // FajtaTextBox
+            // 
+            this.FajtaTextBox.Location = new System.Drawing.Point(120, 12);
+            this.FajtaTextBox.Name = "FajtaTextBox";
+            this.FajtaTextBox.Size = new System.Drawing.Size(100, 20);
+            this.FajtaTextBox.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(227, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(93, 39);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Törlés";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(174, 38);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(75, 23);
+            this.Update.TabIndex = 5;
+            this.Update.Text = "Módosít";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 277);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.FajtaTextBox);
+            this.Controls.Add(this.NevTextBox);
+            this.Controls.Add(this.addButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox NevTextBox;
+        private System.Windows.Forms.TextBox FajtaTextBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button Update;
+    }
+}
+
---
+++ MySql/Form1.cs
@@ -0,0 +1,114 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            using (var conn = new MySqlConnection("Server=localhost;Database=kutya;Uid=root;Pwd="))
+            {
+                conn.Open();
+
+                string nev = NevTextBox.Text;
+                string fajta = FajtaTextBox.Text;
+                DateTime orokbe = dateTimePicker1.Value;
+
+                var lekerdezesCommand = conn.CreateCommand();
+                lekerdezesCommand.CommandText = @"select count(*) from kutyak where nev=@nev";
+                lekerdezesCommand.Parameters.AddWithValue("@nev", nev);
+                long db=(long) lekerdezesCommand.ExecuteScalar();
+                if (db > 0)
+                {
+                    MessageBox.Show("Ilyen kutya mar van");
+                    return;
+                }
+
+                var command = conn.CreateCommand();
+                command.CommandText= @"INSERT INTO `kutya`.`kutyak` (`nev`, `fajta`, `orokbefogadas_datum`) 
+                                VALUES (@nev, @fajta, @orokbe)";
+                command.Parameters.AddWithValue("@nev", nev);
+                command.Parameters.AddWithValue("@fajta", fajta);
+                command.Parameters.AddWithValue("@orokbe", orokbe);
+                command.ExecuteNonQuery();    
+                
+                        
+            };
+            
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            using (var conn = new MySqlConnection("Server=localhost;Database=kutya;Uid=root;Pwd="))
+            {
+                conn.Open();
+                string nev = NevTextBox.Text;
+
+                          
+                var deleteComm = conn.CreateCommand();
+                deleteComm.CommandText = @"DELETE FROM kutyak WHERE nev =@nev";
+                deleteComm.Parameters.AddWithValue("@nev", nev);
+                int num=deleteComm.ExecuteNonQuery();
+
+                if (num == 0)
+                {
+                    MessageBox.Show("Nem volt ilyen adat amit törölni lehetett volna");
+                }
+                else
+                {
+                    MessageBox.Show("Sikeresen törölve");
+                }
+
+            };
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            using (var conn = new MySqlConnection("Server=localhost;Database=kutya;Uid=root;Pwd="))
+            {
+                conn.Open();
+                string nev = NevTextBox.Text;
+                string fajta = FajtaTextBox.Text;
+                DateTime orokbe = dateTimePicker1.Value;
+
+                
+
+                var command = conn.CreateCommand();
+                command.CommandText = @"UPDATE kutyak SET fajta=@fajta, orokbefogadas_datum=@orokbe WHERE nev=@nev"; 
+                               
+                command.Parameters.AddWithValue("@nev", nev);
+                command.Parameters.AddWithValue("@fajta", fajta);
+                command.Parameters.AddWithValue("@orokbe", orokbe);
+                int num=command.ExecuteNonQuery();
+                
+
+                if (num == 0)
+                {
+                    MessageBox.Show("Nem volt ilyen adat amit módostani lehetett volna");
+                }
+                else
+                {
+                    MessageBox.Show("Módosítva");
+                }
+
+            };
+        }
+    }
+}
